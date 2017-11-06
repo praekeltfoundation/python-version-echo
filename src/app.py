9a93b2a7af18348932861f816455b1403ce21cc2
Jason Paidoussi <jason@paidoussi.net>
2017-11-06 10:59:18 +0200

    Added envvar echo

--- src/app.py
+++ src/app.py
@@ -3,13 +3,14 @@ from apistar import environment, typesystem
 from apistar.frameworks.wsgi import WSGIApp as App
 
 
-__version__ = '0.0.9'
+__version__ = '0.0.10'
 VERSION = __version__
 
 
 class Env(environment.Environment):
     properties = {
         'DEBUG': typesystem.boolean(default=False),
+        'ECHO': typesystem.string(default=''),
     }
 
 
@@ -17,11 +18,12 @@ env = Env()
 
 settings = {
     'DEBUG': env['DEBUG'],
+    'ECHO': env['ECHO'],
 }
 
 
 def echo():
-    return {'version': __version__}
+    return {'version': __version__, 'echo': settings['ECHO']}
 
 
 routes = [
